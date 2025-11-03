9749b13d108b7665bae2ec75d369013d3ba68f4b
Rodrigo Faure <rodrigo.s404596@cesurformacion.com>
2025-11-03 12:37:07 +0100

    add entrega

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             AdminPanel panel = new AdminPanel();
-            panel.setVisible(true); // 👈 Esto es clave
-        });
+            panel.setVisible(true);  });
     }
 }
