7ed779f3483628c4e00f114b910cd5a49e149b74
Rodrigo Faure <rodrigo.s404596@cesurformacion.com>
2025-11-03 11:58:52 +0100

    first commit

---
+++ src/main/java/org/example/AdminPanel.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import javax.swing.*;
+
+public class AdminPanel {
+    private JTextArea textArea1;
+    private JLabel lblTitulo;
+    private JLabel lblanadir;
+    private JLabel lblautor;
+    private JButton agregarButton;
+    private JTextField txtnombre;
+    private JTextField txtpais;
+    private JLabel txtplataforma;
+    private JButton btcCerrar;
+    private JTextField txtPlataforma;
+
+    // Constructor
+    public AdminPanel() {
+        // You can add action listeners and other initialization code here
+
+    }
+    //agrgar usuario desde formulario
+    public void addUsuario(){
+
+
+
+
+    }
+
+}
---
+++ src/main/java/org/example/DetalleUsuario.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import javax.swing.*;
+
+public class DetalleUsuario {
+    private JTextArea textArea1;
+}
